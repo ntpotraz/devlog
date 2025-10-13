592f2a7034d36452797576757af8fd5612b1d3f9
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-10-13 07:57:45 -0700

    Gettin some frontend work done

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="devlog">
-      <h1 className="heroText !text-4xl">DevLog</h1>
+    <div className="bg-blue-400 h-full flex flex-col">
+      <h1 className="heroText text-orange-600 text-9xl font-extralight">
+        DevLog
+      </h1>
+      <button
+        type="button"
+        className="text-red-200 text-2xl border-1 rounded-4xl mx-5 py-7 hover:brightness-115 hover:scale-101 hover:transition-all duration-200 ease-in-out"
+        onClick={newNote}
+      >
+        new note
+      </button>
     </div>
   );
+}
+
+function newNote() {
+  console.log("New Note")
 }
 
 export default App;
